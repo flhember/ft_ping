2425fb55a23350d3244cd63f7272bcb34d06309e
Florian HEMBERT <flhember@e1r11p7.clusters.42paris.fr>
2022-11-18 11:46:08 +0100

    Add my lib and basic file to compile

---
+++ includes/ft_ping.h
@@ -0,0 +1,14 @@
+#ifndef FT_PING_H
+# define FT_PING_H
+
+# include "../libft/includes/libft.h"
+# include <sys/socket.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <netinet/in.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+# include <signal.h>
+# include <netinet/ip_icmp.h>
+
+#endif
---
+++ srcs/ft_ping.c
@@ -0,0 +1,8 @@
+#include "../includes/ft_ping.h"
+
+int	main(void)
+{
+	printf("Ping ouai ouai\n");
+
+	return (0);
+}
