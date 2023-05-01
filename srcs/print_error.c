0b5584fdd00d207c7e15baaaf3bb231fd91890f5
flhember <florian.hembertjaj@gmail.com>
2023-05-01 13:59:54 -0500

    add function to have good text error icmp

--- srcs/print_error.c
+++ srcs/print_error.c
@@ -1,10 +1,31 @@
 #include "../includes/ft_ping.h"
 
-static const char       *error_icmp[] = {
-    "",
-    "",
-    "",
-    "Destination Net Unreachable"
+struct code_error
+{
+    int type;
+    int code;
+    char *txt;
+} code_error[] = {
+    {ICMP_DEST_UNREACH, ICMP_NET_UNREACH, "Destination Net Unreachable"},
+    {ICMP_DEST_UNREACH, ICMP_HOST_UNREACH, "Destination Host Unreachable"},
+    {ICMP_DEST_UNREACH, ICMP_PROT_UNREACH, "Destination Protocol Unreachable"},
+    {ICMP_DEST_UNREACH, ICMP_PORT_UNREACH, "Destination Port Unreachable"},
+    {ICMP_DEST_UNREACH, ICMP_FRAG_NEEDED, "Fragmentation needed and DF set"},
+    {ICMP_DEST_UNREACH, ICMP_SR_FAILED, "Source Route Failed"},
+    {ICMP_DEST_UNREACH, ICMP_NET_UNKNOWN, "Network Unknown"},
+    {ICMP_DEST_UNREACH, ICMP_HOST_UNKNOWN, "Host Unknown"},
+    {ICMP_DEST_UNREACH, ICMP_HOST_ISOLATED, "Host Isolated"},
+    {ICMP_DEST_UNREACH, ICMP_NET_UNR_TOS, "Destination Network Unreachable At This TOS"},
+    {ICMP_DEST_UNREACH, ICMP_HOST_UNR_TOS, "Destination Host Unreachable At This TOS"},
+    {ICMP_DEST_UNREACH, ICMP_PKT_FILTERED, "Packet Filtered"},
+    {ICMP_DEST_UNREACH, ICMP_PREC_VIOLATION, "Precedence Violation"},
+    {ICMP_DEST_UNREACH, ICMP_PREC_CUTOFF, "Precedence Cutoff"},
+    {ICMP_REDIRECT, ICMP_REDIR_NET, "Redirect Network"},
+    {ICMP_REDIRECT, ICMP_REDIR_HOST, "Redirect Host"},
+    {ICMP_REDIRECT, ICMP_REDIR_NETTOS, "Redirect Type of Service and Network"},
+    {ICMP_REDIRECT, ICMP_REDIR_HOSTTOS, "Redirect Type of Service and Host"},
+    {ICMP_TIME_EXCEEDED, ICMP_EXC_TTL, "Time to live exceeded"},
+    {ICMP_TIME_EXCEEDED, ICMP_EXC_FRAGTIME, "Frag reassembly time exceeded"}
 };
 
 void            verbose_print(struct icmphdr *icmp)
@@ -29,6 +50,25 @@ void            verbose_print(struct icmphdr *icmp)
         *data, *(data + 1), ntohs(ip->ip_len) - (ip->ip_hl << 2) , *(data + 4), *(data + 5), *(data + 6), *(data + 7));
 }
 
+void            print_icmp_error(int type, int code)
+{
+    int                 i = 0;
+    struct code_error   *tmp;
+
+    tmp = code_error;
+    while (i < 20)
+    {
+        if (tmp->type == type && tmp->code == code)
+        {
+	        printf ("%s\n", tmp->txt);
+	        return ;
+        }
+        i++;
+        tmp++;
+    }
+    printf ("Unknown Code: %d and Type: %d\n", code, type);
+}
+
 void            print_error_rec(struct icmphdr *rec_ping_icmp, struct iphdr	*rec_ping_ip, ssize_t bytes_rec)
 {
     char buf[256];
@@ -39,11 +79,12 @@ void            print_error_rec(struct icmphdr *rec_ping_icmp, struct iphdr	*rec
     getnameinfo((struct sockaddr *)&tmp_addr, sizeof(struct sockaddr_in), buf, sizeof(buf), NULL, 0, NI_NAMEREQD);
     printf("%zu bytes from %s (", bytes_rec - sizeof(struct iphdr), buf);
     return_addr(buf);
-    printf("): %s\n", error_icmp[rec_ping_icmp->type]);
+    printf("): ");
 
-	printf("Error Packet received with ICMP type %d\n", rec_ping_icmp->type);             // for debug
+    print_icmp_error(rec_ping_icmp->type, rec_ping_icmp->code);
+
+	//printf("Error Packet received with ICMP type %d code %d\n", rec_ping_icmp->type, rec_ping_icmp->code);             // for debug
 
     if (t_ping.opt_v)
         verbose_print(rec_ping_icmp);
-
 }
